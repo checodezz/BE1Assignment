b1139ae1b318128bac53a99c94127054d0447f40
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-21 05:12:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { initializeDatabase } = require("./db/db.connect");
+const fs = require("fs");
+const Car = require("./models/car.model");
+
+initializeDatabase();
+
+const jsonData = fs.readFileSync("cars.json", "utf8");
+
+const carsData = JSON.parse(jsonData);
+
+function seedData() {
+  for (const carData of carsData) {
+    const newCar = new Car({
+      brand: carData.brand,
+      model: carData.model,
+      year: carData.year,
+      bodyType: carData.bodyType,
+      fuelType: carData.fuelType,
+      transmission: carData.transmission,
+      mileage: carData.mileage,
+      color: carData.color,
+      price: carData.price,
+      condition: carData.condition,
+      description: carData.description,
+      photos: carData.photos,
+      inMarket: carData.inMarket,
+    });
+    newCar.save();
+    console.log("cars data", newCar.brand);
+  }
+}
+
+seedData();
